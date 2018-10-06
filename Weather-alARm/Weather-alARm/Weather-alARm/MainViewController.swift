15231d69b062c07bd6ed6a4f0b1694d186075177
Zhiling Wang <zhilingwang@ZhilingdeMacBook-Pro.local>
2018-10-06 14:40:42 -0400

    Initial Commit -- Completed the basic structure of the Application.

---
+++ Weather-alARm/Weather-alARm/Weather-alARm/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Weather-alARm
+//
+//  Created by Zhiling Wang on 10/6/18.
+//  Copyright © 2018 Wilson Zhang. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
